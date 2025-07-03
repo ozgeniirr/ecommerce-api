87197c01e994e05713c86fa2505c748b9e46b6a4
ozgeniirr <canozge001@gmail.com>
2025-07-03 17:57:57 +0300

    İyileştirmeler yapıldı

--- server.ts
+++ server.ts
@@ -1,16 +1,7 @@
-import express, { Express } from 'express';
-import dotenv from 'dotenv';
-import authRoutes from './src/api/auth/auth.routes';
-import usersRoutes from './src/api/users/users.routes';
-import productRoutes from "./src/api/products/products.routes"
-dotenv.config();
-const app: Express = express();
-const port = process.env.PORT || 3000;
-app.use(express.json());
-app.use('/api/auth', authRoutes);
-app.use('/api/users', usersRoutes);
-app.use("/api/products",productRoutes);
+import app from "./src/app"; // sadece app import edilir
+
+const port = process.env.PORT || 2000;
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-
--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,16 @@
-// src/app.ts
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+
 import authRoutes from "@/api/auth/auth.routes";
 import userRoutes from "@/api/users/users.routes";
-//import productRoutes from "@/api/products/products.routes"
-
+import productRoutes from "@/api/products/products.routes";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
 
-app.use("/auth", authRoutes);
-app.use("/users", userRoutes); // ← Bu satırı unutma
-//app.use("/api/products",productRoutes);
-app.use(express.json());
-
-// Middleware ve route bağlantıları yapılacak...
-
-export default app; // bu olmazsa app tanınmaz ❌
+export default app;
